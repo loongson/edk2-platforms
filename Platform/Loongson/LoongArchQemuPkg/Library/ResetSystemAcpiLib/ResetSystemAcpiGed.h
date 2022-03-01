45a0dc207656d42c39a6553e40c7f75c65c9b8ea
xianglai li <lixianglai@loongson.cn>
2022-03-01 04:20:54 -0500

    Platform/Loongson: Add Reset System Lib.
    This library provides interfaces related to restart and shutdown.
    
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4054
    
    Cc: Bibo Mao <maobibo@loongson.cn>
    Cc: Chao Li <lichao@loongson.cn>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: xianglai li <lixianglai@loongson.cn>

---
+++ Platform/Loongson/LoongArchQemuPkg/Library/ResetSystemAcpiLib/ResetSystemAcpiGed.h
@@ -0,0 +1,20 @@
+/** @file
+  ResetSystem lib head file.
+
+  Copyright (c) 2021 Loongson Technology Corporation Limited. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+
+typedef struct {
+  UINT64  SleepControlRegAddr;
+  UINT64  SleepStatusRegAddr;
+  UINT64  ResetRegAddr;
+  UINT8   ResetValue;
+} POWER_MANAGER;
+
+extern POWER_MANAGER mPowerManager;
+
